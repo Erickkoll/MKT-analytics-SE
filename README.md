1d4c1a67a349d9caafea60072177dde89a3c21f0
Erick Cordeiro Kollross <erickkollross@gmail.com>
2023-07-24 20:38:03 -0300

    Iniciando o projeto

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MKT-analytics-SE
 Análise das redes sociais do Startup Experiencie com agrupamento de dados
+
---
+++ analytics_Insta_ml.R
@@ -0,0 +1,7 @@
+#Projeto de análise do Instagram do Startup Experience (SE) com agrupamento por clusterização
+
+setwd("C:/Users/erick/OneDrive/Área de Trabalho/Programação/Startup-project/MKT-analytics-SE")
+getwd()
+
+dados <- read.csv("Dados_postagem_MKT - Analytics-CPT.csv")
+View(dados)
